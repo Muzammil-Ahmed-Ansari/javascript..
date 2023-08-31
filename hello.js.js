883f089be8959362fb2d71806b86c5896aace5e5
Muzammil Ahmed Ansari <140959896+Muzammil-Ahmed-Ansari@users.noreply.github.com>
2023-08-31 05:11:43 -0700

    Add files via upload

---
+++ hello.js.js
@@ -0,0 +1,64 @@
+                                                //chapter #1//
+
+//! QUESTION # 1//
+//alert("assalam-u-alaikum")//
+
+//! QUESTION # 2//
+//alert("Error! Please enter a valid password.")//
+
+//! QUESTION # 3//
+// var message = "Welcome to js Land...\n Happy Coding!"//
+// alert(message)//
+
+//! QUESTION # 4//
+// alert("Welcome to js Land...")
+// var message = "Happy Coding!"
+// alert  (message)//
+
+//! QUESTION # 5//
+// var message = "Hello... I can run JS through my web browsers console";
+// document.write(message);//
+
+                                                 // CHAPTER # 2//
+                                                 
+//! QUESTION # 1//
+// var username = "Jhone Doe"
+// document.write(username)
+
+//! QUESTION # 2
+// var myname = "Muzammil Ahmed Ansari"
+// document.write(myname)
+
+//! QUESTION # 3
+// var message = "Hello World"
+// alert(message)
+
+//! QUESTION # 4
+
+//BIODATA//
+// var NAME = "Muzammil Ahmed Ansari"
+// alert(NAME)
+// var age = "21 years old"
+// alert(age)
+// var Qualification = "mobile and web development"
+// alert(Qualification)
+
+//! QUESTION # 5
+// var x = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(x)
+
+//! QUESTION # 6
+// var email = "hafizmuzammil.hm@gmail.com"
+// alert("My email address is " + email)
+
+//! QUESTION # 7
+// var book = "A smarter way to learn Java Script"
+// alert("I am trying to learn from the Book "+ book )
+
+//! QUESTION # 8
+// document.write("Yah! I can write HTML content through Java Script")
+
+//! QUESTION # 9
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(design)
+// document.write("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
